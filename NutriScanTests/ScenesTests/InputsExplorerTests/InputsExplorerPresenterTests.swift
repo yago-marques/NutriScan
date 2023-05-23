b0e9c94663f30c36209b7e03187fcc240a85480d
ThiagoHBA <thiagohhab@gmail.com>
2023-05-23 11:12:28 -0300

    presenter tests

--- NutriScanTests/ScenesTests/InputsExplorerTests/InputsExplorerPresenterTests.swift
+++ NutriScanTests/ScenesTests/InputsExplorerTests/InputsExplorerPresenterTests.swift
@@ -6,30 +6,136 @@
 //
 
 import XCTest
+@testable import NutriScan
+
+typealias InputsExplorerPresenterSUT = (
+    sut: InputsExplorerPresenter,
+    doubles: (InputsExplorerControllerSpy)
+)
 
 final class InputsExplorerPresenterTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_setup_should_inject_controller() {
+        let (sut, controllerSpy) = makeSUTandDoubles()
+        sut.setup(controller: controllerSpy)
+        XCTAssertNotNil(sut.controller)
+    }
+    
+    func test_presentFoodResumeFromImageClassification_when_classify_correctly_should_call_controller() {
+        let (sut, controllerSpy) = makeSUTandDoubles()
+        sut.foods = [Food(name: "laranja", category: "", itens: [])]
+        sut.presentFoodResumeFromImageClassification(response: .init(classification: "(0.61) orange"))
+        XCTAssertEqual(controllerSpy.receivedMessages, [.displayFoodResumeFromImageClassificationCalled])
+    }
+    
+    func test_presentFoodResumeFromImageClassification_when_not_found_food_should_call_controller_with_error() {
+        let (sut, controllerSpy) = makeSUTandDoubles()
+        sut.presentFoodResumeFromImageClassification(response: .init(classification:  "(0.61) orange"))
+        XCTAssertEqual(controllerSpy.receivedMessages, [.displayImageClassificationErrorCalled])
+    }
+    
+    func test_presentFoodResumeFromImageClassification_when_cant_translate_call_controller_with_error() {
+        let (sut, controllerSpy) = makeSUTandDoubles()
+        sut.presentFoodResumeFromImageClassification(response: .init(classification:  "(0.61) a"))
+        XCTAssertEqual(controllerSpy.receivedMessages, [.displayImageClassificationErrorCalled])
+    }
+    
+    func test_presentFoodGrid_should_populate_foods() {
+        let (sut, _) = makeSUTandDoubles()
+        sut.presentFoodGrid(response: .init(foods: [RemoteFood(name: "orange", category: "", itens: [])]))
+        XCTAssertFalse(sut.foods.isEmpty)
+    }
+    
+    func test_presentFoodGrid_should_call_controller() {
+        let (sut, controllerSpy) = makeSUTandDoubles()
+        sut.presentFoodGrid(response: .init(foods: [RemoteFood(name: "orange", category: "", itens: [])]))
+        XCTAssertEqual(controllerSpy.receivedMessages, [.displayFoodGridCalled])
+    }
+    
+    func test_presentFilteredFoods_when_valid_category_should_filter_correctly() {
+        let (sut, _) = makeSUTandDoubles()
+        let inputedFood = Food(name: "laranja", category: "fruta", itens: [])
+        sut.foods = [inputedFood]
+        sut.presentFilteredFoods(response: .init(filterBy: "Fruta"))
+        XCTAssertFalse(sut.filteredFoods.isEmpty)
+    }
+    
+    func test_presentFilteredFoods_when_all_should_not_filter() {
+        let (sut, _) = makeSUTandDoubles()
+        let inputedFood = Food(name: "laranja", category: "", itens: [])
+        sut.foods = [inputedFood]
+        sut.presentFilteredFoods(response: .init(filterBy: "Todos"))
+        XCTAssertFalse(sut.filteredFoods.isEmpty)
+    }
+    
+    func test_presentFilteredFoods_should_call_controller() {
+        let (sut, controllerSpy) = makeSUTandDoubles()
+        let inputedFood = Food(name: "laranja", category: "", itens: [])
+        sut.foods = [inputedFood]
+        sut.presentFilteredFoods(response: .init(filterBy: "Todos"))
+        XCTAssertEqual(controllerSpy.receivedMessages, [.displayFilteredFoodsCalled])
+    }
+    
+    func test_presentSearchedFoods_should_search() {
+        let (sut, _) = makeSUTandDoubles()
+        sut.filteredFoods = [Food(name: "laranja", category: "", itens: [])]
+        sut.presentSearchedFoods(response: .init(searchBy: "la"))
+        XCTAssertFalse(sut.searchedFoods.isEmpty)
+    }
+    
+    func test_presentSearchedFoods_should_search_correctly() {
+        let (sut, _) = makeSUTandDoubles()
+        sut.filteredFoods = [Food(name: "laranja", category: "", itens: [])]
+        sut.presentSearchedFoods(response: .init(searchBy: "la"))
+        XCTAssertEqual(sut.searchedFoods.first!.name, "laranja")
+    }
+    
+    func test_presentSearchedFoods_should_call_controller() {
+        let (sut, controllerSpy) = makeSUTandDoubles()
+        sut.filteredFoods = [Food(name: "laranja", category: "", itens: [])]
+        sut.presentSearchedFoods(response: .init(searchBy: "la"))
+        XCTAssertEqual(controllerSpy.receivedMessages, [.displaySearchedFoodsCalled])
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func test_presentConnectivityError_should_call_controller() {
+        let (sut, controllerSpy) = makeSUTandDoubles()
+        sut.presentConnectivityError()
+        XCTAssertEqual(controllerSpy.receivedMessages, [.displayConnectivityErrorCalled])
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_presentEmptyListNotificationIfNeeded_when_empty_should_call_controller_correctly() {
+        let (sut, controllerSpy) = makeSUTandDoubles()
+        sut.searchedFoods = []
+        sut.presentEmptyListNotificationIfNeeded()
+        XCTAssertEqual(controllerSpy.receivedMessages, [.displayEmptyListNotificationCalled])
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_presentEmptyListNotificationIfNeeded_when_not_empty_should_call_controller_correctly() {
+        let (sut, controllerSpy) = makeSUTandDoubles()
+        sut.searchedFoods = [Food(name: "laranja", category: "", itens: [])]
+        sut.presentEmptyListNotificationIfNeeded()
+        XCTAssertEqual(controllerSpy.receivedMessages, [.hideEmptyListNotificationCalled])
     }
+    
+    func test_presentFoodResumeFromVoice_when_get_from_voice_should_call_controller_correctly() {
+        let (sut, controllerSpy) = makeSUTandDoubles()
+        let inputedFoodName = "laranja"
+        sut.foods = [Food(name: inputedFoodName, category: "", itens: [])]
+        sut.presentFoodResumeFromVoice(response: .init(text: inputedFoodName))
+        XCTAssertEqual(controllerSpy.receivedMessages, [.displayFoodResumeFromVoiceCalled])
+    }
+    
+    func test_presentFoodResumeFromVoice_when_cant_get_from_voice_should_call_controller_correctly() {
+        let (sut, controllerSpy) = makeSUTandDoubles()
+        sut.foods = []
+        sut.presentFoodResumeFromVoice(response: .init(text: "laranja"))
+        XCTAssertEqual(controllerSpy.receivedMessages, [.displayVoiceRecognizerErrorCalled])
+    }
+}
 
+extension InputsExplorerPresenterTests {
+    func makeSUTandDoubles() -> InputsExplorerPresenterSUT {
+        let spy = InputsExplorerControllerSpy()
+        let sut = InputsExplorerPresenter(controller: spy)
+        return (sut, (spy))
+    }
 }
